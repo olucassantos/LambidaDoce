8330b6cbf1bdce6ae7deb4e9e3495e94f3874964
Lucas Anjos <oargus.g@gmail.com>
2025-09-25 22:23:46 -0300

    Refactor routes to use AdminController for admin home route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin', 'home')->name('admin.home');
 });
